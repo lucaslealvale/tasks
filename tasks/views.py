535166af15b5f043987c42d327f7ff39976c7a8d
lucaslealvale <lucaslealvale01@gmail.com>
2020-11-20 13:50:19 -0300

    ccc

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Task, TaskSerializer
+from .models import Task, TaskSerializer
 from django.shortcuts import render,redirect
 
 # Create your views here.
