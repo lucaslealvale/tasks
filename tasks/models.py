6ddcd0d671f247c4f2a5efd52670485ff0cce38b
lucaslealvale <lucaslealvale01@gmail.com>
2020-11-20 12:08:21 -0300

    aaaa

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,15 @@
-from django.db import models
-
+from rest_framework import serializers
+from .models import ItemModel
+ 
 # Create your models here.
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    description = models.CharField(max_length=500)
+class Task(serializers.Model):
+    title = serializers.CharField(max_length=200)
+    pub_date = serializers.DateTimeField('date published')
+    description = serializers.CharField(max_length=500)
+
+class TaskForm(serializers.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'pub_date','description']
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import StudentModel
+from .forms import StudentForm
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+def CreateView(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/data')
+    else:
+        form =StudentForm()
+        context = {
+            'form':form
+        }
+        return render(request,'create.html',context)
