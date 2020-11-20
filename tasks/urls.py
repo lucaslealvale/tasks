17d24e8a1017e2876e1a3534568ee573b2802cd5
lucaslealvale <lucaslealvale01@gmail.com>
2020-11-20 15:17:41 -0300

    dddd

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('tarefas', views.getTask, name='getTask')
-
+    path('tarefas', views.getTask, name='getTask'),
 ]
